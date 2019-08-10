df5684a824c4aaa51ebe4bb55d8247637d0544bb
htam <anonimozurik@gmail.com>
2019-08-10 12:50:50 -0300

    Updates

---
+++ precache-manifest.b76aae8deba2105ed99ef2f91eb83f78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5316568751d6b84194d3999f81233e5c",
+    "url": "/htam00/geekdev.github.io/index.html"
+  },
+  {
+    "revision": "6a0c3ac3a5183e3f57a3",
+    "url": "/htam00/geekdev.github.io/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "b8ea082488d8d4f6f2e3",
+    "url": "/htam00/geekdev.github.io/static/js/2.1063d617.chunk.js"
+  },
+  {
+    "revision": "6a0c3ac3a5183e3f57a3",
+    "url": "/htam00/geekdev.github.io/static/js/main.5add8e61.chunk.js"
+  },
+  {
+    "revision": "db896345ecd8d33ae379",
+    "url": "/htam00/geekdev.github.io/static/js/runtime~main.c258e96d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/htam00/geekdev.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
